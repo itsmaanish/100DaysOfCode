69413cc2fd87c7e2b7dc477ba7627b7739063a40
MANISH KUMAR <92860884+itsmaanish@users.noreply.github.com>
2023-04-14 21:19:28 +0530

    Day 09

---
+++ ReverseLinkedList.java
@@ -0,0 +1,14 @@
+class ReverseLinkedList  {
+    public ListNode reverseList(ListNode head) {
+        ListNode cur = head;
+        ListNode prev = null;
+
+        while(cur!= null){
+            ListNode next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next; 
+        }
+        return prev;
+    }
+}
