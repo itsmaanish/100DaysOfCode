9c73a4a5bf0ae2722f59f485f6420d859d28c3d9
MANISH KUMAR <92860884+itsmaanish@users.noreply.github.com>
2023-04-25 21:06:14 +0530

    Day 20

---
+++ palnidromic.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int countSubstrings(String s) {
+        int count=0;
+        String str="";
+        for(int i=0; i<s.length();i++){
+            str="";
+            for(int j=i; j<s.length();j++){
+                Character ch=s.charAt(j);
+                str+=ch;
+                boolean bool=isPalindrome(str);
+                if(bool) count++;
+            }
+        }
+        return count;
+    }
+    public boolean isPalindrome(String str){
+        int i=0,j=str.length()-1;
+        while(i<=j){
+            char a=str.charAt(i);
+            char b=str.charAt(j);
+            if(a!=b) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
